c4d1e65df550f3b76c0401b3a0e97b2e1df974a6
Juanjo Morell <34169360+JuanjoMorell@users.noreply.github.com>
2020-12-22 18:10:03 +0100

    Final Practica 3 a falta de problema en testquant

--- Practica 3/testdct.m
+++ Practica 3/testdct.m
@@ -1,11 +1,32 @@
-function testdct(fname)
+function [mse, dmaxdifer] = testdct(fname)
 
-    % Se lee el archivo de imagen y se genera la matriz truecolor RGB
+% Aplica la transformada DCT y la transformada inversa de DCT
+% sobre una imagen.
+
+% ENTRADAS:
+%   fname : Nombre de la imagen a transformar.
+% SALIDAS:
+%   mse : Error cuadratico medio entre matrices original y reconstruida.
+%   dmaxdifer : Maxima diferencia entre pixeles homologos.
+
+    % Se lee el archivo de imagen y se genera la matriz truecolor RGB.
+    % Se convierte el espacio RGB a un espacio YCbCr.
+    % Se amplia la matriz a dimensiones multiplo de 8.
     [X, Xamp, ~, m, n, ~, ~, ~] = imlee(fname);
     
+    % Aplica DCT sobre la matriz ampliada.
+    % Se obtiene la matriz transformada XTRANS.
     Xtrans = imdct(Xamp);
+    
+    % Aplica iDCT sobre XTRANS
+    % Se obtiene la reconstruccion de la matriz 
+    % original ampliada, XAMPREC.
     Xamprec = imidct(Xtrans, m, n);
     
+    % Se convierte XAMPREC al espacio de color RGB.
+    % Se reduce sus dimensiones al espacio original.
+    % Se obtiene la matriz reconstruida XREC.
+    % Se almacena en una imagen del tipo BMP.
     [Xrec, ~] = imescribe(Xamprec, m, n, fname);
     
     %Calcular el error cuadratico medio MSE
@@ -16,19 +37,21 @@ function testdct(fname)
     % Calcular maxima diferencia entre pixeles
     ddifer=abs(double(Xrec)-double(X));
     dmaxdifer=max(max(max(ddifer)));
-    disp("Maxima diferencia entre pixeles");
+    disp("Máxima diferencia entre pixeles");
     disp(dmaxdifer);
     
     % Test visual
+    % Se visualiza la imagen X
     [m,n,~] = size(X);
     figure('Units','pixels','Position',[100 100 n m]);
     set(gca,'Position',[0 0 1 1]);
     image(X);
     set(gcf,'Name','Imagen original X');
+    
+    % Se visualiza la imagen XREC
     figure('Units','pixels','Position',[100 100 n m]);
     set(gca,'Position',[0 0 1 1]);
     image(Xrec);
     set(gcf,'Name','Imagen reconstruida Xrec');
     
 end
-
--- Practica 3/testquant.m
+++ Practica 3/testquant.m
@@ -1,13 +1,46 @@
 function testquant(fname,caliQ)
-    
+
+% Funcion que cuantiza y descuantiza la transformada DCT de una imagen.
+
+% ENTRADAS:
+%   fname: Imagen de entrada
+%   caliQ: Factor de calidad (entero positivo >= 1)
+% SALIDAS:
+%   mse : Error cuadratico medio entre matrices original y reconstruida.
+%   dmaxdifer : Maxima diferencia entre pixeles homologos.
+
+    % Se lee el archivo de imagen y se genera la matriz truecolor RGB.
+    % Se convierte el espacio RGB a un espacio YCbCr.
+    % Se amplia la matriz a dimensiones multiplo de 8.
     [X, Xamp, ~, m, n, ~, ~, ~] = imlee(fname);
     
+    % --- PROCESO DE CUANTIZACION ---
+    
+    % Aplica DCT sobre la matriz ampliada.
+    % Se obtiene la matriz transformada XTRANS.    
     Xtrans = imdct(Xamp);
+    
+    % Se cuantiza la matriz transformada XTRANS, mediante dos matrices
+    % de cuantizacion, llamadas QY y QC, multiplicadas por CALIQ.
+    % Se obtiene la matriz cuantizada XLAB.
     Xlab = quantmat(Xtrans,caliQ);
+    
+    % --- PROCESO DE DESCUANTIZACION ---
+    
+    % Se descuantiza la matriz XLAB, aplicando el proceso inverso
+    % que en la cuantizacion.
+    % Se obtiene la matriz descuantizada XTRANSREC, aun transformada.
     Xtransrec = desquantmat(Xlab, caliQ);
     
+    % Aplica iDCT sobre XTRANS
+    % Se obtiene la reconstruccion de la matriz 
+    % original ampliada, XAMPREC.
     Xamprec = imidct(Xtransrec, m, n);
     
+    % Se convierte XAMPREC al espacio de color RGB.
+    % Se reduce sus dimensiones al espacio original.
+    % Se obtiene la matriz reconstruida XREC.
+    % Se almacena en una imagen del tipo BMP.    
     [Xrec, ~] = imescribe(Xamprec, m, n, fname);
     
     %Calcular el error cuadratico medio MSE
@@ -32,4 +65,3 @@ function testquant(fname,caliQ)
     image(Xrec);
     set(gcf,'Name','Imagen reconstruida Xrec');
 end
-
